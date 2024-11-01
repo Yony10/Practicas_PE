e8e0a8cf87786aff8ffe844e75fed8d7d6cc65f5
Yony10 <benitezalexander661@gmail.com>
2024-11-01 11:46:56 -0600

    practica12

---
+++ Practica_12/ejercicio2.js
@@ -0,0 +1,11 @@
+class Ciclista {
+    constructor(distanciaRecorrida) {
+        this._distanciaRecorrida = distanciaRecorrida;
+    }
+
+    get distanciaRecorrida (){
+        if (this._distanciaRecorrida  > 0 ) {
+            
+        }
+    }
+}
